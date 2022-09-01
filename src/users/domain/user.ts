0e14c02c8d20eddafd487f22121634c57d0a6dea
postBG <profile2697@gmail.com>
2022-09-01 15:03:47 +0900

    feat: add value equals

--- src/users/domain/user.ts
+++ src/users/domain/user.ts
@@ -1,10 +1,11 @@
-import { ArgumentNullError } from './error';
+import { ArgumentNullError } from '../../utils/errors';
+import { ValueObject } from '../../utils/domain/value-object';
 
-export class UserName {
+export class UserName implements ValueObject<UserName> {
   private readonly value: string;
 
   constructor(value: string) {
-    if (value == null)
+    if (value === null || value === undefined)
       throw new ArgumentNullError('value is null or undefined');
 
     if (value.length < 3)
@@ -14,16 +15,24 @@ export class UserName {
 
     this.value = value;
   }
+
+  equals(other: UserName): boolean {
+    return this.value === other.value;
+  }
 }
 
-export class UserId {
+export class UserId implements ValueObject<UserId> {
   private readonly value: string;
 
   constructor(value: string) {
-    if (value == null)
+    if (value === null || value === undefined)
       throw new ArgumentNullError('value is null or undefined');
 
     this.value = value;
+  }
+
+  equals(other: UserId): boolean {
+    return this.value === other.value;
   }
 }
 
